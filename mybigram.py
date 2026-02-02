71d465c0b4646db92c844b997c08dfd783c24cb8
Ellery <alyssampark99@gmail.com>
2026-02-02 00:42:02 -0500

    Converted all characters to lowercase; maps bigrams to the tensor

---
+++ mybigram.py
@@ -0,0 +1,12 @@
+import torch
+
+words = open('indian_names.txt', 'r').read().splitlines()
+
+b = {}
+for w in words:
+  chs = ['<S>'] + list(w) + ['<E>']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    bigram = (ch1, ch2)
+    b[bigram] = b.get(bigram, 0) + 1
+print(b)
+
