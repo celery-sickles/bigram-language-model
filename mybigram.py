1b8476c36e7daeab53cd7921da02246e0e6ceeeb
Ellery <alyssampark99@gmail.com>
2026-02-02 00:43:19 -0500

    same as last time but i hope mybigram commits this time

--- mybigram.py
+++ mybigram.py
@@ -1,7 +1,10 @@
 import torch
 
+#read in the dataset
 words = open('indian_names.txt', 'r').read().splitlines()
+words = [w.lower() for w in words]
 
+#create a dictionary of the two-character sequences and their frequencies
 b = {}
 for w in words:
   chs = ['<S>'] + list(w) + ['<E>']
@@ -9,4 +12,23 @@ for w in words:
     bigram = (ch1, ch2)
     b[bigram] = b.get(bigram, 0) + 1
 print(b)
+
+
+#create empty tensor
+N = torch.zeros((27, 27), dtype=torch.int32)
+
+#maps characters to index, adds 0 as "start"
+#accounts for edge cases that dont have all 26 characters
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
+
+#counts bigrams and updates the tensor
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    N[ix1, ix2] += 1
 
